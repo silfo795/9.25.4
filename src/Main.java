f253f7ebed92d1771ffd7aaf9392dfedccb15109
silfo795 <a143050@gmail.com>
2017-09-25 15:54:31 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	// what the f*ck
+        /*幹你娘
+        廢
+         */
+        int a,b,c,d,e;
+        int x,y;
+        Scanner in=new Scanner(System.in);
+        x=in.nextInt();
+        y=in.nextInt();
+        System.out.printf("%d+%d=%d\n",x,y,x+y);
+        System.out.printf("%d-%d=%d\n",x,y,x-y);
+        System.out.printf("%d*%d=%d\n",x,y,x*y);
+        System.out.printf("%d/%d=%d...%d\n",x,y,x/y,x%y);
+        in.close();
+    }
+}
